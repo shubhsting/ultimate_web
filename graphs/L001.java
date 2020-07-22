ebbc573b19a069704d3c5cc63261735b5695ecba
Shubham <shubhamsingh1840@gmail.com>
2020-07-22 15:48:46 +0530

    updated

--- graphs/L001.java
+++ graphs/L001.java
@@ -27,15 +27,64 @@ public class L001 {
         }
     }
 
-    public static void main(String[] args) {
-        ArrayList<Graph_Edge>[] graph = new ArrayList[7];
-        for (int i = 0; i < 7; i++)
-            graph[i] = new ArrayList<>();
-        addEdge(graph, 1, 2, 0);
-        addEdge(graph, 3, 5, 10);
-        addEdge(graph, 6, 4, 7);
-        display(graph);
+    public static int findEdge(ArrayList<Graph_Edge>[] graph, int node1, int node2) {
+        int index = 0;
+        for (int i = 0; i < graph[node1].size(); i++) {
+            if (graph[node1].get(i).dest == node2)
+                return index = i;
+        }
+        return -1;
     }
 
-    
+    public static void removeEdge(ArrayList<Graph_Edge>[] graph, int node1, int node2) {
+        int idx1 = findEdge(graph, node1, node2);
+        int idx2 = findEdge(graph, node2, node1);
+        graph[node1].remove(idx1);
+        graph[node2].remove(idx2);
+    }
+
+    public static void removenode(ArrayList<Graph_Edge>[] graph, int node) {
+        for (int i = 0; i < graph[node].size(); i++)
+            removeEdge(graph, node, graph[node].get(i).dest);
+    }
+
+    public static boolean hasPath(ArrayList<Graph_Edge>[] graph, int start, int end, boolean[] vis) {
+        if (start == end)
+            return true;
+        vis[start] = true;
+        boolean res = false;
+        for (Graph_Edge ed : graph[start])
+            if (!vis[ed.dest])
+                res = res || hasPath(graph, ed.dest, end, vis);
+        return res;
+    }
+
+    public static int allPath(ArrayList<Graph_Edge>[] graph, int start, int end, boolean[] vis) {
+        if (start == end)
+            return 1;
+        vis[start] = true;
+        int count = 0;
+        for (Graph_Edge ed : graph[start])
+            if (!vis[ed.dest])
+                count += allPath(graph, ed.dest, end, vis);
+        vis[start] = false;
+        return count;
+    }
+
+    public static void main(String[] args) {
+        // ArrayList<Graph_Edge>[] graph = new ArrayList[7];
+        // for (int i = 0; i < 7; i++)
+        // graph[i] = new ArrayList<>();
+        // addEdge(graph, 1, 2, 0);
+        // addEdge(graph, 3, 5, 10);
+        // addEdge(graph, 6, 4, 7);
+        // display(graph);
+        int[] arr = { 10, 20, 30 };
+        for (int i = 0; i < 3; i++)
+            System.out.print(arr[i] + " ");
+
+        for (int ele : arr)
+            System.out.println(ele + " ");
+    }
+
 }
