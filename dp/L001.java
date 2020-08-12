1f61ffb78c93e075feb8f0c8edd0487b749b68e5
Shubham <shubhamsingh1840@gmail.com>
2020-08-12 15:30:08 +0530

    updated

--- dp/L001.java
+++ dp/L001.java
@@ -1,9 +1,6 @@
 import java.util.*;
 
 public class L001 {
-    public static void main(String[] args) {
-
-    }
 
     // memoization
     public static int fibonacci(int num, int[] dp) {
@@ -94,5 +91,76 @@ public class L001 {
             dp[sp] = count;
         }
         return dp[0];
+    }
+
+    // memoization
+    public static int friends_pairing_problem(int num, int[] dp) {
+        if (num <= 1)
+            return dp[num] = 1;
+        if (dp[num] != 0)
+            return dp[num];
+        int single = friends_pairing_problem(num - 1, dp);
+        int paiup = friends_pairing_problem(num - 2, dp) * (num - 1);
+        return dp[num] = single + paiup;
+    }
+
+    public static int friends_pairing_problem_DP(int num, int[] dp) {
+        int N = num;
+        for (num = 0; num <= N; num++) {
+            if (num <= 1) {
+                dp[num] = 1;
+                continue;
+            }
+
+            int single = dp[num - 1];
+            int paiup = dp[num - 2] * (num - 1);
+            dp[num] = single + paiup;
+        }
+        return dp[N];
+    }
+
+    public static int partition_into_k_subsets(int num, int k, int[][] dp) {
+        if (num < k)
+            return dp[num][k] = 0;
+        if (num == k || k == 1)
+            return dp[num][k] = 1;
+
+        if (dp[num][k] != 0)
+            return dp[num][k];
+        int newSet = partition_into_k_subsets(num - 1, k - 1, dp);
+        int existingSet = partition_into_k_subsets(num - 1, k, dp) * k;
+        return dp[num][k] = newSet + existingSet;
+    }
+    // mobilekeypadproblem gfg dp 3d int[][][] dp
+
+    // coinchange permutation
+    public static int coinchangeper(int[] arr, int tar, int[] dp) {
+        if (tar == 0)
+            return dp[tar] = 1;
+        if (dp[tar] != 0)
+            return dp[tar];
+        int count = 0;
+        for (int ele : arr) {
+            if (tar - ele >= 0)
+                count += coinchangeper(arr, tar - ele, dp);
+        }
+        return dp[tar] = count;
+    }
+
+    public static int coinchange_DP(int[] arr, int tar, int[] dp) {
+        int N = tar;
+        for (tar = 0; tar < N; tar++) {
+            if (tar == 0) {
+                dp[tar] = 1;
+                continue;
+            }
+            int count = 0;
+            for (int ele : arr) {
+                if (tar - ele >= 0)
+                    count += dp[tar - ele];// coinchangeper(arr, tar - ele, dp);
+            }
+            dp[tar] = count;
+        }
+        return dp[N];
     }
 }
